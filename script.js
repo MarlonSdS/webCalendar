1697aa17fc95de3f74ca093e0a8c30fbbccec61d
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-08-22 10:35:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//function to get the actual date and save in difernte variable that will be use in the entire project
+function getDate(){
+    globalThis.date = new Date();
+    globalThis.year = Date.prototype.getFullYear.toString();
+
+}
+
+    console.log(year.toString);
+    console.log(date.toString)
