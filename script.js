268df0631aee309fe12a6a32b9395c5f225ff696
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-08-23 15:08:08 -0300

    passando os meses

--- script.js
+++ script.js
@@ -1,9 +1,98 @@
-//function to get the actual date and save in difernte variable that will be use in the entire project
-function getDate(){
-    globalThis.date = new Date();
-    globalThis.year = Date.prototype.getFullYear.toString();
 
+let data = new Date();
+//object to get the actual date and save in difernte variable that will be use in the entire project
+    let getDate = {
+        year: data.getFullYear(),
+        month: getMes(data.getMonth()),
+        day: data.getDate(),
+        feira: data.getDay()
+    }
+// function to convert the number returned by getMonth into string
+function getMes(object){
+    switch (object) {
+        case 0:
+            return "January"
+            break;
+        case 1:
+            return "February"
+            break;
+        case 2:
+            return "March"
+            break;
+        case 3:
+            return "April"
+            break;
+        case 4:
+            return "May"
+            break;
+        case 5:
+            return "June"
+            break;
+        case 6:
+            return "July"
+            break;
+        case 7:
+            return "August"
+            break;
+        case 8:
+            return "September"
+            break;
+        case 9:
+            return "October"
+            break;
+        case 10:
+            return "November"
+            break;
+        case 11:
+            return "December"
+            break;
+        default:
+            break;
+    }
 }
-
-    console.log(year.toString);
-    console.log(date.toString)
+let mesNow = data.getMonth();
+let mesNowStr = getMes(mesNow)
+//changes to the previous month
+function mesAnterior(){
+    let data = new Date();
+   
+    if(mesNowStr == getDate.month){
+       console.log(data.getMonth() - 1)
+        getDate.month = getMes(mesNow - 1)
+       
+        console.log(getDate.month) 
+    }else{
+        
+        if(mesNow < 0){
+            mesNow = 12;
+        }else{
+            mesNow = mesNow - 1;
+        }
+        getDate.month = getMes(mesNow - 1)
+    }
+    
+   construirHeader()
+}
+//changes to the next month
+function mesSeguinte(){
+    let data = new Date();
+   
+    if(mesNowStr == getDate.month){
+       console.log(data.getMonth() + 1)
+        getDate.month = getMes(mesNow + 1)
+       
+        console.log(getDate.month) 
+    }else{
+        mesNow = mesNow + 1;
+        getDate.month = getMes(mesNow + 1)
+    }
+    
+   construirHeader()
+}
+//create the words in the header, like the month name and the number of the year
+function construirHeader(){
+    var a = document.getElementById('ano')
+    
+    var b = getDate.month + ", " + getDate.year
+    a.innerHTML = b
+}
